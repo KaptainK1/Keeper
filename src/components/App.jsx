c90a4a55ff8c2211f882ffe308f92b2b455b291d
Dylan Hoffman <32976792+KaptainK1@users.noreply.github.com>
2021-04-12 16:28:44 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Note Heading</h1>
+      <p>Note Paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
